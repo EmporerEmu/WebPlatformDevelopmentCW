db775f49a474a0fc4af885bfbb1c8257e34a48e7
EmporerEmu <47246665+EmporerEmu@users.noreply.github.com>
2021-05-02 14:53:55 +0100

    Edited fitness schedule layout

--- public/js/addActiveClass.js
+++ public/js/addActiveClass.js
@@ -1,9 +1,10 @@
+// This function will allow the navbar to show the current page by changing css classes on the links
 $(function ($) {
 	let url = window.location.href;
 	$("li a").each(function () {
 		if (this.href === url) {
 			$(this).closest("a").addClass("link-secondary");
-            $(this).closest("a").removeClass("link-dark")
+			$(this).closest("a").removeClass("link-dark");
 		}
 	});
 });
