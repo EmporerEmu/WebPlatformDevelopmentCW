2b6f67afe1b70474d8d1e2bde381d5e2b1168e7d
EmporerEmu <47246665+EmporerEmu@users.noreply.github.com>
2021-05-06 17:39:27 +0100

    Changed the datetime field for the Tasks db object.
    Changed the date field from a string to date in UTC format. This enables
    the browser to use this date when going into the edit task function.

--- models/tasksModels.js
+++ models/tasksModels.js
@@ -12,80 +12,86 @@ class Tasks {
 			this.db = new nedb();
 		}
 	}
-
+	// new Date(date).toLocaleDateString("en-gb"),
+	// "26/04/2021"
 	// Seeding tasks into the database
 	init() {
 		this.db.insert({
-			name: "Last month test",
-			type: "Swimming",
-			date: "26/04/2021",
+			name: "I don't understand",
+			type: "Running",
+			date: new Date('31 May 2021 12:00 UTC').toISOString().substring(0, 10),
 			completed: true,
 			username: "ann@gmail.com",
 		});
 		this.db.insert({
-			name: "Last month test",
-			type: "Swimming",
-			date: "27/04/2021",
-			completed: true,
-			username: "ann@gmail.com",
-		});
-		this.db.insert({
-			name: "Last month test",
-			type: "Swimming",
-			date: "01/05/2021",
-			completed: true,
-			username: "ann@gmail.com",
-		});
-		this.db.insert({
-			name: "Last month test",
-			type: "Swimming",
-			date: "02/05/2021",
-			completed: true,
-			username: "ann@gmail.com",
-		});
-		this.db.insert({
-			name: "Morning Cardio",
-			type: "Swimming",
-			date: "03/05/2021",
-			completed: true,
-			username: "ann@gmail.com",
-		});
-		console.log("DB Task inserted");
-		this.db.insert({
-			name: "Flying session",
-			type: "Something else",
-			date: "04/05/2021",
-			completed: false,
-			username: "ann@gmail.com",
-		});
-		console.log("DB Task inserted");
-		this.db.insert({
-			name: "Leg Day",
+			name: "Like, really don't understand",
 			type: "Lifting",
-			date: "05/05/2021",
-			completed: false,
+			date: "2021/04/27",
+			date: new Date("27 May 2021 12:00 UTC").toISOString().substring(0, 10),
+			completed: true,
 			username: "ann@gmail.com",
 		});
-		console.log("DB Task inserted");
 		this.db.insert({
-			name: "New Year New Me",
-			type: "Something else",
-			date: "06/05/2021",
-			completed: false,
+			name: "?",
+			type: "Swimming",
+			date: "2021/05/01",
+			date: new Date("1 May 2021 12:00 UTC")
+				.toISOString()
+				.substring(0, 10),
+			completed: true,
 			username: "ann@gmail.com",
 		});
-		console.log("DB Task inserted");
+		this.db.insert({
+			name: "Morning Jog",
+			type: "Running",
+			// date: "02/05/2021",
+			date: new Date("2 May 2021 12:00 UTC")
+				.toISOString()
+				.substring(0, 10),
+			completed: true,
+			username: "ann@gmail.com",
+		});
+		// this.db.insert({
+		// 	name: "Morning Cardio",
+		// 	type: "Swimming",
+		// 	date: "03/05/2021",
+		// 	completed: true,
+		// 	username: "ann@gmail.com",
+		// });
+		// console.log("DB Task inserted");
+		// this.db.insert({
+		// 	name: "Flying session",
+		// 	type: "Something else",
+		// 	date: "04/05/2021",
+		// 	completed: false,
+		// 	username: "ann@gmail.com",
+		// });
+		// console.log("DB Task inserted");
+		// this.db.insert({
+		// 	name: "Leg Day",
+		// 	type: "Lifting",
+		// 	date: "05/05/2021",
+		// 	completed: false,
+		// 	username: "ann@gmail.com",
+		// });
+		// console.log("DB Task inserted");
+		// this.db.insert({
+		// 	name: "New Year New Me",
+		// 	type: "Something else",
+		// 	date: "06/05/2021",
+		// 	completed: false,
+		// 	username: "ann@gmail.com",
+		// });
+		// console.log("DB Task inserted");
 	}
 
 	addTask(name, type, date, username) {
-		// var duration = endTime.getHours() - startTime.getHours();
-		// console.log(duration);
 		var entry = {
 			name: name,
 			type: type,
-			date: new Date(date).toLocaleDateString("en-gb"),
+			date: new Date(date).toISOString().substring(0, 10),
 			username: username,
-            completed: false,
+			completed: false,
 		};
 		console.log("Entry created", entry);
 
@@ -117,7 +123,7 @@ class Tasks {
 				if (err) {
 					reject(err);
 				} else {
-                    resolve(docRem);
+					resolve(docRem);
 					console.log("Document removed from db");
 				}
 			});
@@ -143,7 +149,7 @@ class Tasks {
 				if (err) {
 					reject(err);
 				} else {
-                    vali.sortByDateDesc(doc);
+					// vali.sortByDateAsc(doc);
 					resolve(doc);
 					console.log("document got", doc);
 				}
@@ -166,7 +172,7 @@ class Tasks {
 				function (err, doc) {
 					if (err) {
 						reject(err);
-                        console.log("Document not updated", err);
+						console.log("Document not updated", err);
 					} else {
 						resolve(doc);
 						console.log("Document updated: ", doc);
